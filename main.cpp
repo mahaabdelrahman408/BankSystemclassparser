6911991d0a3614a165bbd9fadeaee6116ea67608
Fatma-Elfar <fatmaelfar13@gmail.com>
2024-09-17 08:55:03 +0300

    3-FileManager class

--- main.cpp
+++ main.cpp
@@ -239,14 +239,60 @@ public:
         admin.setSalary(stod(info[3]));
         return admin;
     }
-
-
-
-
-
-
 };
 
+class FileManager {
+private:
+    const string clientsFilePath = "clients.txt";
+    const string employeesFilePath = "employees.txt";
+    const string adminsFilePath = "admins.txt";
+
+    void writeToFile(const string& filePath, const string& data);
+    std::vector<std::string> readFromFile(const string& filePath);
+
+public:
+    void addClient(const Client& client);
+    void addEmployee(const Employee& employee);
+    void addAdmin(const Admin& admin);
+
+    std::vector<Client> getAllClients();
+    std::vector<Employee> getAllEmployees();
+    std::vector<Admin> getAllAdmins();
+
+    void removeAllClients();
+    void removeAllEmployees();
+    void removeAllAdmins();
+};
+
+void FileManager::addClient(const Client& client) {
+    string clientData = client.getName() + "," + to_string(client.getId()) + "," + client.getPassward() + "," + to_string(client.getBalance());
+    writeToFile(clientsFilePath, clientData);
+}
+
+void FileManager::writeToFile(const string& filePath, const string& data) {
+    ofstream file(filePath, ios::app);
+    if (file.is_open()) {
+        file << data << endl;
+        file.close();
+    } else {
+        cerr << "Not found this file" << filePath << endl;
+    }
+}
+
+std::vector<std::string> FileManager::readFromFile(const string& filePath) {
+    std::vector<std::string> data;
+    ifstream file(filePath);
+    if (file.is_open()) {
+        string line;
+        while (getline(file, line)) {
+            data.push_back(line);
+        }
+        file.close();
+    } else {
+        cerr << "Error opening file: " << filePath << endl;
+    }
+    return data;
+}
 
 
 
@@ -279,5 +325,10 @@ int main()
     a.setPassword("14567859");
     a.setSalary(70000);
     a.display();
+
+     FileManager fileManager;
+Client client;
+fileManager.addClient(client);
+
     return 0;
 }
